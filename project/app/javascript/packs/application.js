e9703b0adc57047751c092436617176739cddcb9
wpark <woolim.park@hotmail.com>
2020-07-03 23:24:47 +0200

    test

--- project/app/javascript/packs/application.js
+++ project/app/javascript/packs/application.js
@@ -7,10 +7,8 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-require("jquery");
 require("backbone");
-require("bootstrap");
-require("./src/application.scss");
+require("bootstrap/dist/js/bootstrap");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
