e8e5552a8a44e0526f1f751a5c4cc47e43f9c266
wpark <woolim.park@hotmail.com>
2020-07-20 01:11:29 +0200

    turbolinks removed

--- project/app/javascript/packs/application.js
+++ project/app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 var jQuery = require("jquery");
@@ -22,6 +21,7 @@ require("backbone");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
 import SPA from "./SPA.js"
 $(()=> {
 	new SPA();
