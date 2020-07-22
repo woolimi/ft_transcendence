0cb45414235a4b12104cc48ea4183e695baa0526
wpark <woolimi91@naver.com>
2020-07-22 15:11:08 +0200

    data fetch

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -9,13 +9,15 @@ class User < ApplicationRecord
   def self.from_omniauth(auth, session_id)
     user = User.find_by(ft_id: auth[:uid])
     if user.blank?
+      small_img = auth[:info][:image];
+      small_img[small_img.rindex("/")] = "/small_"
       user = User.create(
         ft_id: auth[:uid], 
-        session_id: session_id)        
+        session_id: session_id)
       user.create_user_profile(
         name: auth[:info][:name],
         nickname: auth[:info][:nickname],
-        avatar_url: auth[:info][:image])
+        avatar_url: small_img)
     end
     return user
   end
