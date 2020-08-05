12d022f8b9567e858a4e376fe45bff95bd375ba3
wpark <woolimi91@naver.com>
2020-08-05 08:38:00 +0200

    chat ui

--- project/config/webpack/environment.js
+++ project/config/webpack/environment.js
@@ -9,4 +9,11 @@ environment.plugins.prepend(
     Backbone: "backbone.js",
   })
 );
+const aliasConfig = {
+    'jquery': 'jquery-ui-dist/external/jquery/jquery.js',
+    'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
+};
+
+environment.config.set('resolve.alias', aliasConfig);
+
 module.exports = environment;
