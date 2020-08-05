87e6c28471c7d4b7955a259629cc64121937fe51
Mohit ASHAR <mohitashar1995@gmail.com>
2020-08-05 17:42:58 +0200

    Adding delete

--- project/db/migrate/20200805100430_add_two_factor_to_user_profile.rb
+++
@@ -1,6 +0,0 @@
-class AddTwoFactorToUserProfile < ActiveRecord::Migration[6.0]
-  def change
-    add_column :user_profiles, :two_factor, :boolean, default: false
-    add_column :user_profiles, :block_list, :string, array: true, default: []
-  end
-end
