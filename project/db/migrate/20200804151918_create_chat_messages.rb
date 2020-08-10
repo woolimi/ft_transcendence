7c69e4a32d1b8bb899c4ae1a983c612488fc634c
Woolim Park <44322175+woolimi@users.noreply.github.com>
2020-08-11 00:11:53 +0200

    Feature/chat (#17)
    * scroll event with  20 messages fetch finished
    
    * block user finished

--- project/db/migrate/20200804151918_create_chat_messages.rb
+++ project/db/migrate/20200804151918_create_chat_messages.rb
@@ -1,6 +1,6 @@
 class CreateChatMessages < ActiveRecord::Migration[6.0]
   def change
-    create_table :chat_messages, id: :uuid do |t|
+    create_table :chat_messages do |t|
       t.text  :content, null: false
       t.timestamp :timestamp, null: false
       t.references :chat, type: :uuid, foreign_key: true
