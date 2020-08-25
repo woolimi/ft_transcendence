e9031a147cc26eb3c812c10943bcd202346f2392
Nicolas Klarsfeld <nicolas.klarsfeld@gmail.com>
2020-08-25 15:34:52 +0200

    tournament list looks ok with flexbox, now need to do css/html of single tournament page

--- project/app/javascript/packs/Tournaments.js
+++ project/app/javascript/packs/Tournaments.js
@@ -17,13 +17,30 @@ $(() => {
 		render: function () {
 			// const content = this.template();
 			let tournaments = this.getTournaments();
-			let content = '<ul>'
+			let content = $('<div/>',{
+				class: 'tournamentList'
+			})
 			for(const tournament of tournaments){
-				content += `<li><a href='#tournaments/${tournament.id}'>
-				            ${tournament.name}</li>`
+				// content += `<li><a href='#tournaments/${tournament.id}'>
+				//             ${tournament.name}</li>`
+				let contentElem = $('<div/>',{
+					class: 'tournamentItem'
+				})
+				contentElem.on('click', ()=>{
+					location.href = `#tournaments/${tournament.id}`;
+				})
+				contentElem.html(tournament.name)
+				if(tournament.status == 'finished'){
+					contentElem.css('background-color','#7c7e7c')
+					contentElem.css('border','#484848 5px solid')
+				} else if (tournament.status == 'running'){
+					contentElem.css('background-color', '#19d81f')
+					contentElem.css('border', '5px solid #008814')
+				}
+				content.append(contentElem)
 			}
-			content += '</ul>'
-			this.$el.html(content);
+			this.$el.html('');
+			this.$el.append(content)
 			return this;
 		}
 	});
