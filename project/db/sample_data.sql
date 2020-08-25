862d0024beb5c758e8442b4a83740db92b871c22
pvineet44 <pvineet44@gmail.com>
2020-08-25 14:48:35 +0200

    sample data

--- project/db/sample_data.sql
+++ project/db/sample_data.sql
@@ -29,4 +29,4 @@ insert into wars(guild_1, guild_2, guild_1_score, guild_2_score, guild_1_matches
 values ('3a6ea760-9d93-4313-8e88-e562fe528aef', '05db40c5-96b8-4c9b-824f-cf90d2089a4b', 5, 0, 1, 0, 0, 0, 1, 0, current_timestamp, current_timestamp, 100, '{307e9b4d-a9a9-47c4-acfd-56e006c6b1f8,a529a440-72f7-4073-a4cf-9cea8d44bbb7}', current_timestamp, current_timestamp);
 
 
-select * from wars
+select * from wars  
