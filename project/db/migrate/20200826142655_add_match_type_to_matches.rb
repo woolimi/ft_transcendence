8ff57b46b94a6bea164b0af2c61f3d3108d5d6c4
pvineet44 <pvineet44@gmail.com>
2020-08-26 18:52:16 +0200

    war page

--- project/db/migrate/20200826142655_add_match_type_to_matches.rb
+++ project/db/migrate/20200826142655_add_match_type_to_matches.rb
@@ -5,7 +5,5 @@ class AddMatchTypeToMatches < ActiveRecord::Migration[6.0]
     rename_column :matches, :player_left, :player_1
     rename_column :matches, :player_right, :player_2
     remove_column :wars, :match_list
-
-
   end
 end
