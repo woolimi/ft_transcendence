78e422e3bccb72ddabf229cc8d6c78e5e4bfdd04
pvineet44 <pvineet44@gmail.com>
2020-08-26 17:05:24 +0200

    schema changes

--- project/db/migrate/20200826142655_add_match_type_to_matches.rb
+++ project/db/migrate/20200826142655_add_match_type_to_matches.rb
@@ -2,5 +2,10 @@ class AddMatchTypeToMatches < ActiveRecord::Migration[6.0]
   def change
     add_column :matches, :match_type, :string
     add_reference :matches, :war,  type: :uuid, foreign_key: true
+    rename_column :matches, :player_left, :player_1
+    rename_column :matches, :player_right, :player_2
+    remove_column :wars, :match_list
+
+
   end
 end
