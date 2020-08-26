34c4b8293a2808ec2b2d58dae035cc5a728bd221
pvineet44 <pvineet44@gmail.com>
2020-08-26 23:50:52 +0200

    inner html fix

--- project/app/javascript/packs/War.js
+++ project/app/javascript/packs/War.js
@@ -59,8 +59,8 @@ $(() => {
                 var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
                 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-                document.getElementById("clock").innerHTML = days + "d " + hours + "h " +
+                if (document.getElementById("clock"))
+                    document.getElementById("clock").innerHTML = days + "d " + hours + "h " +
                     minutes + "m " + seconds + "s ";
 
                 if (distance < 0) {
