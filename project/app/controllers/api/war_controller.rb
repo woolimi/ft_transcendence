5094443266b55bc5e0edf6de5bfa2d029f43df14
pvineet44 <pvineet44@gmail.com>
2020-08-27 15:01:13 +0200

    timer fix

--- project/app/controllers/api/war_controller.rb
+++ project/app/controllers/api/war_controller.rb
@@ -5,10 +5,15 @@ class Api::WarController < ApplicationController
         if (params[:user_id] == current_user[:id])
             user = UserProfile.find_by(user_id: current_user[:id]).as_json(only: [:guild_id])
 
+            puts ">>>>>>>>>>>>>>>>>>>>>>>"
+            puts user
+            puts ">>>>>>>>>>>>>>>>>>>>>>>"
             guild = Guild.find_by(id: user["guild_id"]).as_json(only: [:war_id])
             
-            war = War.find_by(id: guild["war_id"]).as_json(only: [:guild_1, :guild_2, :guild_1_score, :guild_2_score, :start_date, :end_date, :match_ongoing])
             
+            war = War.find_by(id: guild["war_id"]).as_json(only: [:guild_1, :guild_2, :guild_1_score, :guild_2_score, :start_date, :end_date, :match_ongoing, :])
+            
+
             user_guild_number = check_guild_number(war, user["guild_id"])
             if ((war[user_guild_number + "_score"] === war["guild_1_score"]) && (war["guild_1_score"] > war["guild_2_score"]))
                 war["position"] = "win"
@@ -21,7 +26,7 @@ class Api::WarController < ApplicationController
             else
                 war["position"] = "draw"
             end
-            war["user_guild_number"] = user_guild_number
+            war["user_guild_name"] = war[user_guild_number+"_name"]
             return render json: war, status: :ok
         else
 			return render plain: "Forbidden", status: :forbidden 
--- project/app/javascript/packs/Router.js
+++ project/app/javascript/packs/Router.js
@@ -14,66 +14,67 @@ import WarHistory from "./WarHistory.js"
 
 const Router = {};
 if ($('html').data().isLogin) {
-	$(() => {
-		const remove_channel = function() {
-			if (ChatChannel.channel)
-				ChatChannel.unsubscribe();
-			if (ChannelChannel.channel)
-				ChannelChannel.unsubscribe();
-				
-			if (Chat.content)
-				Chat.content.undelegateEvents();
-			if (Channel.content)
-				Channel.content.undelegateEvents();
-		};
+    $(() => {
+        const remove_channel = function() {
+            if (ChatChannel.channel)
+                ChatChannel.unsubscribe();
+            if (ChannelChannel.channel)
+                ChannelChannel.unsubscribe();
+            if (Chat.content)
+                Chat.content.undelegateEvents();
+            if (Channel.content)
+                Channel.content.undelegateEvents();
+            if (War.content)
+                clearInterval(War.content.intervalId);
+        };
 
-		const RouterClass = Backbone.Router.extend({
-			routes: {
-				"": "game",
-				"game": "game",
-				"profile": "profile",
-				"guild": "guild",
-				"guild/war_history/:guild_id": "guildHistory",
-				"chats/:room": "chat",
-				"channels/:channel_id": "channel",
-				"war" :"war"
-			},
-			game: function () {
-				remove_channel();
-				Game.content.render();
-			},
-			profile: function () {
-				remove_channel();
-				Profile.content = new Profile.Content();
-				Profile.content.render();
-			},
-			guild: function () {
-				remove_channel();
-				Guild.content.render();
+        const RouterClass = Backbone.Router.extend({
+            routes: {
+                "": "game",
+                "game": "game",
+                "profile": "profile",
+                "guild": "guild",
+                "guild/war_history/:guild_id": "guildHistory",
+                "chats/:room": "chat",
+                "channels/:channel_id": "channel",
+                "war": "war"
+            },
+            game: function() {
+                remove_channel();
+                Game.content.render();
+            },
+            profile: function() {
+                remove_channel();
+                Profile.content = new Profile.Content();
+                Profile.content.render();
+            },
+            guild: function() {
+                remove_channel();
+                Guild.content.render();
 
-			},
-			guildHistory: function(guild_id){
-				WarHistory.histView = new WarHistory.HistContent({guild_id:guild_id});
-			},
-			chat: function (room) {
-				remove_channel();
-				Chat.content = new Chat.Content({ room: room });
-			},
-			channel: function (channel_id) {
-				remove_channel();
-				Channel.content = new Channel.Content({ channel_id: channel_id });
-			},
-			war: function () {
-				War.content.render();	
-			}
-		});
-		const router = new RouterClass();
-		Router.router = router;
+            },
+            guildHistory: function(guild_id) {
+                WarHistory.histView = new WarHistory.HistContent({ guild_id: guild_id });
+            },
+            chat: function(room) {
+                remove_channel();
+                Chat.content = new Chat.Content({ room: room });
+            },
+            channel: function(channel_id) {
+                remove_channel();
+                Channel.content = new Channel.Content({ channel_id: channel_id });
+            },
+            war: function() {
+                War.content = new War.Content();
+            }
+        });
+        const router = new RouterClass();
+        Router.router = router;
 
-		Router.router.on("route", function (curRoute, params) {
-			Navbar.currentRoute.set({ route: curRoute });
-		});
-	});
+        Router.router.on("route", function(curRoute, params) {
+            Navbar.currentRoute.set({ route: curRoute });
+        });
+    });
 }
 
 export default Router;
