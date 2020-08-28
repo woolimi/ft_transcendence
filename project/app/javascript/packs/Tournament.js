2f1257322e63eb6ef126584d32b05f941e6b4b9a
Nicolas Klarsfeld <nicolas.klarsfeld@gmail.com>
2020-08-28 18:44:02 +0200

    single tournament displayed using a Backbone template

--- project/app/javascript/packs/Tournament.js
+++ project/app/javascript/packs/Tournament.js
@@ -15,8 +15,13 @@ $(() => {
 
 	Tournament.Content = Backbone.View.extend({
 		el: $("#view-content"),
+		page_template: _.template($("script[name='tmpl-tournament-page']").html()),
+		info_template: _.template($("script[name='tmpl-tournament-infos']").html()),
+		tree_template: _.template($("script[name='tmpl-tournament-tree']").html()),
+
 		model_backbone: {},
 		model:{},
+
 		initialize: async function(id){
 			this.id = id
 			this.model_backbone = new TournamentModel({id: id})
@@ -27,122 +32,31 @@ $(() => {
 				this.playerNames[this.model.players[i].id] 
 					= this.model.players[i].name
 			}
-			this.render()
+			this.render_page()
+			this.render_infos()
+			this.render_tree()
 		},
 
-		getPlayerName: function(id){ // todo : fetch
-			// let playerNames = ['', 'Tom', 'John', 'Cena', 'Max']
-			// debugger
-			return this.playerNames[id]
+		render_page() {
+			this.$el.html(this.page_template());
 		},
-
-		getWinnerClass: function(winnerId, id){
-			if(winnerId == id)
-				return 'winner'
-			else
-				return ''
+		render_infos() {
+			this.$el.find('#tournament-infos').html(
+				this.info_template({model: this.model})
+			)
 		},
-
-		getTournament: function(){
-			// todo : model and fetch it
-			let tournament = {
-				id: this.model.id,
-				name: this.model.name,
-				status: this.model.status,
-				id_player_1: this.model.players[0].id,
-				id_player_2: this.model.players[1].id,
-				id_player_3: this.model.players[2].id,
-				id_player_4: this.model.players[3].id,
-				semiFinal_1: {
-					match_id: this.model.semis[0].id,
-					left_score: this.model.semis[0].player_one.points,
-					right_score: this.model.semis[0].player_two.points,
-					winner_id: this.model.semis[0].winner
-				},
-				semiFinal_2:{
-					match_id: this.model.semis[1].id,
-					left_score: this.model.semis[1].player_one.points,
-					right_score: this.model.semis[1].player_two.points,
-					winner_id: this.model.semis[1].winner
-				},
-				final:{
-					match_id: this.model.final.id,
-					left_score: this.model.final.player_one.points,
-					right_score: this.model.final.player_two.points,
-					winner_id: this.model.final.winner
-				}
-			}
-			return tournament
+		render_tree() {
+			this.$el.find('#tournament-tree').html(
+				this.tree_template({
+					model: this.model,
+					playerNames: this.playerNames
+				})
+			)
 		},
-
-		// template: _.template($("script[name='tmpl-tournaments-list']").html()),
-		render: function() {
-			// const content = this.template();
-			// todo : model, use id to get specific tournament
-			let tournament = this.getTournament(); 
-			console.log(tournament)
-			let content = `<div id="tournamentBody">
-			<h3>Tournament</h3><br>
-			<p> <strong>Tournament name:</strong> ${tournament.name} </p>
-			<p> <strong>Status:</strong> ${tournament.status}</p>
-		<div id="tournamentMain">
-    <ul class="round round-3">
-        <li class="spacer">&nbsp;</li>
-        
-				<li class="gameTour game-top 
-					${this.getWinnerClass(tournament.semiFinal_1.winner_id, tournament.id_player_1)}">
-					${this.model.players[0].name} 
-					<span>${tournament.semiFinal_1.left_score}</span>
-				</li>
-        <li class="gameTour game-spacer">&nbsp;</li>
-				<li class="gameTour game-bottom 
-					${this.getWinnerClass(tournament.semiFinal_1.winner_id, tournament.id_player_2)}">
-					${this.model.players[1].name} 
-					<span>${tournament.semiFinal_1.right_score}</span>
-				</li>
-
-        <li class="spacer">&nbsp;</li>
-        
-				<li class="gameTour game-top 
-					${this.getWinnerClass(tournament.semiFinal_2.winner_id, tournament.id_player_3)}">
-					${this.model.players[2].name}
-					<span>${tournament.semiFinal_2.left_score}</span>
-				</li>
-        <li class="gameTour game-spacer">&nbsp;</li>
-				<li class="gameTour game-bottom 
-					${this.getWinnerClass(tournament.semiFinal_2.winner_id, tournament.id_player_4)}">
-					${this.model.players[3].name}
-					<span>${tournament.semiFinal_2.right_score}</span>
-				</li>
-
-        <li class="spacer">&nbsp;</li>
-    </ul>
-    <ul class="round round-4">
-        <li class="spacer">&nbsp;</li>
-        
-				<li class="gameTour game-top 
-					${this.getWinnerClass(tournament.final.winner_id, tournament.semiFinal_1.winner_id)}">
-					${this.getPlayerName(tournament.semiFinal_1.winner_id)}
-					<span>${tournament.final.left_score}</span>
-				</li>
-        <li class="gameTour game-spacer">&nbsp;</li>
-				<li class="gameTour game-bottom 
-					${this.getWinnerClass(tournament.final.winner_id, tournament.semiFinal_2.winner_id)}">
-					${this.getPlayerName(tournament.semiFinal_2.winner_id)} 
-					<span>${tournament.final.right_score}</span>
-				</li>
-        
-        <li class="spacer">&nbsp;</li>
-    </ul>       
-</div>
-</div>
-			`
-			this.$el.html(content);
-			return this;
-		}
 	});
 	
 })
 
 }
+
 export default Tournament;
