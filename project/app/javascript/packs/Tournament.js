fe0ae245d7c5bf6549db313e5df6d99970163ec0
Nicolas Klarsfeld <nicolas.klarsfeld@gmail.com>
2020-08-29 14:26:41 +0200

    list participants and clock. i will need to do join/quit next.

--- project/app/javascript/packs/Tournament.js
+++ project/app/javascript/packs/Tournament.js
@@ -19,26 +19,26 @@ $(() => {
 		info_template: _.template($("script[name='tmpl-tournament-infos']").html()),
 		tree_template: _.template($("script[name='tmpl-tournament-tree']").html()),
 		match_template: _.template($("script[name='tmpl-tournament-match']").html()),
-		
-		model_backbone: {},
-		model:{},
+		participants_template: _.template($("script[name='tmpl-tournament-participants']").html()),
+
+		model: {},
 
 		initialize: async function(id){
 			this.id = id
-			this.model_backbone = new TournamentModel({id: id})
-			await Helper.fetch(this.model_backbone)
-			this.model = this.model_backbone.attributes
+			this.model = new TournamentModel({id: id})
+			await Helper.fetch(this.model)
+			console.log(this.model.attributes)
 			this.playerNames = {}
 			for(let i=0; i < 4; i++){
-				if(this.model.players[i])
-					this.playerNames[this.model.players[i].id] = this.model.players[i].name
+				if(this.model.attributes.players[i])
+					this.playerNames[this.model.attributes.players[i].id] = this.model.attributes.players[i].name
 			}
 			this.render_page()
 			this.render_infos()
 			this.render_tree()
-			this.render_match('#semi-final-1', this.model.semis[0])
-			this.render_match('#semi-final-2', this.model.semis[1])
-			this.render_match('#final', this.model.final)
+			this.render_timer(this.model.attributes.registration_end)
+			this.render_join_button()
+			this.render_participant_list()
 		},
 
 		render_page() {
@@ -46,13 +46,16 @@ $(() => {
 		},
 		render_infos() {
 			this.$el.find('#tournament-infos').html(
-				this.info_template({model: this.model})
+				this.info_template({model: this.model.attributes})
 			)
 		},
 		render_tree() {
 			this.$el.find('#tournament-tree').html(
 				this.tree_template()
 			)
+			this.render_match('#semi-final-1', this.model.attributes.semis[0])
+			this.render_match('#semi-final-2', this.model.attributes.semis[1])
+			this.render_match('#final', this.model.attributes.final)
 		},
 		render_match(selector, match){
 			this.$el.find(selector).html(
@@ -61,7 +64,40 @@ $(() => {
 					playerNames: this.playerNames
 				})
 			)
-		}
+		},
+		render_timer(end_date) {
+			this.intervalId = setInterval(function() {
+					var dds = new Date(end_date);
+					var countDownDate = dds.getTime();
+					var now = Date.now();
+
+					var distance = countDownDate - now;
+					var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+					var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+					var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+					var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+					if (document.getElementById("clock"))
+							document.getElementById("clock").innerHTML = days + "d " + hours + "h " +
+							minutes + "m " + seconds + "s ";
+
+					if (distance < 0) {
+							clearInterval(this.intervalId);
+							document.getElementById("clock").innerHTML = "Registration time has passed.";
+					}
+			}, 1000);
+		},
+		render_join_button(){
+			// 3 cases : 
+			// started (== 4 players already) : can do nothing
+			// not started yet and not joined : can join
+			// not started yet and joined : can quit
+			
+		},
+		render_participant_list(){
+			this.$el.find('#participant-list').html(
+				this.participants_template({users: this.model.attributes.players})
+			)
+		},
 	});
 	
 })
