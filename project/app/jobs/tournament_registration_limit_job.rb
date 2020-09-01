d179fea08141dfcd40a2771672f9af166b7059ae
nicokla <nicolas.klarsfeld@gmail.com>
2020-09-01 16:24:48 +0200

    join/quit disappear when match starts (#45)

--- project/app/jobs/tournament_registration_limit_job.rb
+++ project/app/jobs/tournament_registration_limit_job.rb
@@ -4,5 +4,6 @@ class TournamentRegistrationLimitJob < ApplicationJob
   def perform(tournament)
     return tournament.cancel() if tournament.players.count < 4
     tournament.launch()
+    ActionCable.server.broadcast "tournament_#{tournament.id}_channel", tournament.jbuild()
   end
 end
