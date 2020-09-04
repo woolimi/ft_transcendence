2cb7ccb06d3ec8cba018cafbaab2aa3c01c51d14
nicokla <nicolas.klarsfeld@gmail.com>
2020-09-04 14:26:04 +0200

    ban user works (#51)

--- project/app/models/user_profile.rb
+++ project/app/models/user_profile.rb
@@ -1,4 +1,10 @@
 class UserProfile < ApplicationRecord
 	belongs_to :user
 	validates :nickname, uniqueness: true
+
+	def self.search(query)
+		where.not(user_id: current_user[:id])
+		.where("lower(name) LIKE ? OR lower(nickname) LIKE ?", "%#{sanitize_sql_like(query)}%", "%#{sanitize_sql_like(query)}%")
+		.as_json(only: [:user_id, :name, :nickname, :avatar_url])
+	end
 end
